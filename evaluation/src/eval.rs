33f842e0882e571b9a59f24d706e6c88c348638e
InterestingBrainPoops <satasiyahet@gmail.com>
2022-02-01 12:59:45 -0800

    Add distance to nearest food eval param

--- evaluation/src/eval.rs
+++ evaluation/src/eval.rs
@@ -1,11 +1,23 @@
-use board::small::SmallRequest;
+use board::{board::Coordinate, small::SmallRequest};
 
 pub trait StaticEval {
     fn static_eval(&self) -> i32;
 }
 
+fn manhattan_dist(p1: &Coordinate, p2: &Coordinate) -> i32 {
+    (p1.x - p2.x).abs() + (p1.y - p2.y).abs()
+}
+
 impl StaticEval for SmallRequest {
     fn static_eval(&self) -> i32 {
-        (self.board.snakes[self.you].length) as i32 * 10
+        let mut dist_food = i32::MAX;
+        let you_head = self.board.snakes[self.you].head.clone();
+        for food in &self.board.food {
+            if manhattan_dist(&you_head, food) < dist_food {
+                dist_food = manhattan_dist(&you_head, food);
+            }
+        }
+
+        (self.board.snakes[self.you].length) as i32 * 10 - dist_food * 500
     }
 }
