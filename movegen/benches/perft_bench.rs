3b192942693a411b984d3e83637c05160ef2e235
InterestingBrainPoops <satasiyahet@gmail.com>
2022-02-15 16:57:22 -0800

    Added benchmark for perft

---
+++ movegen/benches/perft_bench.rs
@@ -0,0 +1,46 @@
+use std::fs;
+
+use board::{board::GameRequest, small::SmallRequest};
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use movegen::{
+    genmove::GenMove,
+    makeunmake::{MakeUnmake, Move},
+};
+// use criterion::{black_box, criterion_group, criterion_main, Criterion};
+
+fn perft(state: &mut SmallRequest, depth: u8, player: bool, you_move: Option<Move>) {
+    // println!("e");
+    if depth == 0 || state.amount_alive() == 1 {
+        return;
+    }
+    // println!("{:?}", state.snake_moves(state.you));
+    if player {
+        for x in state.snake_moves(state.you) {
+            perft(state, depth, !player, Some(x));
+        }
+    } else {
+        let mut delta;
+        for x in state.all_snake_moves(you_move.unwrap()) {
+            // let t0 = state.clone();
+            delta = state.make_move(&x);
+            perft(&mut delta, depth - 1, !player, None);
+            state.unmake_move(&delta);
+            // assert_eq!(state.board.snakes, t0.board.snakes);
+        }
+    }
+}
+
+fn criterion_benchmark(c: &mut Criterion) {
+    let contents =
+        fs::read_to_string(env!("CARGO_MANIFEST_DIR").to_string() + "\\tests\\food.json")
+            .expect("Something went wrong reading the file");
+    // make it into a smallrequest
+    let seralized: GameRequest = serde_json::from_str(&contents).expect("Invalid json");
+    let mut small = seralized.into_small();
+    c.bench_function("perft5food", |b| {
+        b.iter(|| perft(&mut small, 5, true, None))
+    });
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
