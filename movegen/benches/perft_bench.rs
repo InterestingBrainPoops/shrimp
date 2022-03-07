0c125403bb05115f02e15a2dd4ff96a784563bfd
InterestingBrainPoops <satasiyahet@gmail.com>
2022-03-06 16:34:03 -0800

    fix some compilation errors, and add more benchmarks

--- movegen/benches/perft_bench.rs
+++ movegen/benches/perft_bench.rs
@@ -4,7 +4,7 @@ use board::{board::GameRequest, small::SmallRequest};
 use criterion::{criterion_group, criterion_main, Criterion};
 use movegen::{
     genmove::GenMove,
-    makeunmake::{MakeUnmake, Move},
+    makeunmake::{Direction, MakeUnmake, Move},
 };
 // use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
@@ -36,17 +36,71 @@ fn perft(state: &mut SmallRequest, depth: u8, player: bool, you_move: Option<Mov
     }
 }
 
-fn criterion_benchmark(c: &mut Criterion) {
+fn movegen_perft_midgame(c: &mut Criterion) {
+    let contents =
+        fs::read_to_string(env!("CARGO_MANIFEST_DIR").to_string() + "\\tests\\movegen.json")
+            .expect("Something went wrong reading the file");
+    // make it into a smallrequest
+    let seralized: GameRequest = serde_json::from_str(&contents).expect("Invalid json");
+    let mut small = seralized.into_small();
+    c.bench_function("movegen_perft_midgame", |b| {
+        b.iter(|| perft(&mut small, 5, true, None))
+    });
+}
+
+fn movegen_perft_earlygame(c: &mut Criterion) {
     let contents =
         fs::read_to_string(env!("CARGO_MANIFEST_DIR").to_string() + "\\tests\\food.json")
             .expect("Something went wrong reading the file");
     // make it into a smallrequest
     let seralized: GameRequest = serde_json::from_str(&contents).expect("Invalid json");
     let mut small = seralized.into_small();
-    c.bench_function("perft5food", |b| {
+    c.bench_function("movegen_perft_early", |b| {
         b.iter(|| perft(&mut small, 5, true, None))
     });
 }
 
-criterion_group!(benches, criterion_benchmark);
+fn move_make_midgame(c: &mut Criterion) {
+    let contents =
+        fs::read_to_string(env!("CARGO_MANIFEST_DIR").to_string() + "\\tests\\movegen.json")
+            .expect("Something went wrong reading the file");
+    // make it into a smallrequest
+    let seralized: GameRequest = serde_json::from_str(&contents).expect("Invalid json");
+    let mut small = seralized.into_small();
+    let move_to_make = small.all_snake_moves(Move {
+        id: 0,
+        direction: Direction::Up,
+    })[0];
+    c.bench_function("move_make_midgame", |b| {
+        b.iter(|| {
+            let delta = small.make_move(&move_to_make);
+            small.unmake_move(&delta);
+        })
+    });
+}
+
+fn movegen_midgame(c: &mut Criterion) {
+    let contents =
+        fs::read_to_string(env!("CARGO_MANIFEST_DIR").to_string() + "\\tests\\movegen.json")
+            .expect("Something went wrong reading the file");
+    // make it into a smallrequest
+    let seralized: GameRequest = serde_json::from_str(&contents).expect("Invalid json");
+    let small = seralized.into_small();
+    c.bench_function("movegen_midgame", |b| {
+        b.iter(|| {
+            small.all_snake_moves(black_box(Move {
+                id: 0,
+                direction: Direction::Up,
+            }))
+        })
+    });
+}
+
+criterion_group!(
+    benches,
+    movegen_perft_midgame,
+    movegen_perft_earlygame,
+    move_make_midgame,
+    movegen_midgame
+);
 criterion_main!(benches);
